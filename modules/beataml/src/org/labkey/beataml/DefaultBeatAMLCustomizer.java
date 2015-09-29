077dc8100d57e688d320d552bf7d9d41a0be4f8a
bbimber <bbimber@gmail.com>
2015-09-29 14:03:29 -0700

    stub of beataml module

---
+++ modules/beataml/src/org/labkey/beataml/DefaultBeatAMLCustomizer.java
@@ -0,0 +1,16 @@
+package org.labkey.beataml;
+
+import org.labkey.api.data.TableInfo;
+import org.labkey.api.ldk.table.AbstractTableCustomizer;
+
+/**
+ * Created by bimber on 9/25/2015.
+ */
+public class DefaultBeatAMLCustomizer extends AbstractTableCustomizer
+{
+    @Override
+    public void customize(TableInfo ti)
+    {
+
+    }
+}
